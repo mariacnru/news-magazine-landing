a8e8a9b9450783155cf0b781c7467e0e042551cc
maria <mariacnru@gmail.com>
2026-05-28 10:24:26 +0330

    categories bg modified

--- src/Pages/Home/components/Categories/components/CategoryCard.jsx
+++ src/Pages/Home/components/Categories/components/CategoryCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function CategoryCard({ title, bgColor }) {
   return (
-    <div className="border border-white/30 bg-white/30 backdrop-blur-lg py-2 px-8 text-xs rounded-lg">
+    <div className="border border-gray-200 bg-white/50 backdrop-blur-lg py-2 px-8 text-xs rounded-lg">
       <span>{title}</span>
     </div>
   );
